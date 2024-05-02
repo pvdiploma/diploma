057e52d4f07160032b87911ae5b35df249d5e129
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-05-02 23:24:05 +0400

    add event models and migrations

---
+++ internal/domain/models/event.go
@@ -0,0 +1,25 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID           int64
+	OwnerID      int64
+	Name         string
+	Description  string
+	Country      string
+	City         string
+	Place        string
+	Date         time.Time
+	TicketAmount uint32
+	Age          string
+	Categories   []EventCategory
+}
+
+type EventCategory struct {
+	ID       int64
+	EventID  int64
+	Category string
+	Price    uint32
+	Amount   uint32
+}
